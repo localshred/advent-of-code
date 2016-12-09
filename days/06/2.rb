4e3ea624a0d12c1b427f1b611a025bf7f9a98c80
BJ Neilsen <bj.neilsen@gmail.com>
2016-12-08 22:39:13 -0700

    6.1 and 6.2 solutions

---
+++ days/06/2.rb
@@ -0,0 +1,79 @@
+require 'digest/md5'
+
+def convert(input)
+  input
+    .map(&:strip)
+    .select { |line| !line.empty? }
+end
+
+PUZZLE = convert(File.readlines(File.expand_path("input.txt", __dir__)))
+
+TESTS = [
+  [
+    convert(%q{
+      eedadn
+      drvtee
+      eandsr
+      raavrd
+      atevrs
+      tsrnev
+      sdttsa
+      rasrtv
+      nssdts
+      ntnada
+      svetve
+      tesnvt
+      vntsnd
+      vrdear
+      dvrsen
+      enarar
+    }.lines),
+    'advent',
+  ],
+]
+
+def count_chars(accumulator, line)
+  line.chars.each_with_index.reduce(accumulator) do |inner, (char, index)|
+    inner[index][char] += 1
+    inner
+  end
+end
+
+def most_frequent_char(accumulator, (index, chars))
+  accumulator[index] = chars.min_by { |char, count| count }.first
+  accumulator
+end
+
+def solve(input)
+  initial = Hash.new do |outer, index|
+    outer[index] = Hash.new do |inner, char|
+      inner[char] = 0
+    end
+  end
+
+  input
+    .reduce(initial, &method(:count_chars))
+    .reduce(Array.new(input.first.size), &method(:most_frequent_char))
+    .join
+end
+
+def test(label, input, expected)
+  $stdout.write "[#{label}]: expected #{expected}"
+  actual = solve(input)
+  $stdout.write ", actual #{actual.inspect}"
+  if actual == expected
+    $stdout.write " ✅"
+  else
+    $stdout.write " 💥"
+  end
+  puts
+end
+
+def run
+  TESTS.each_with_index do |(input, expected), index|
+    test("Test #{index}", input, expected)
+  end
+  puts "[Puzzle]: #{solve(PUZZLE)}"
+end
+
+run
